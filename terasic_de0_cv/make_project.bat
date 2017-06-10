d2327f0184bb7a83b1a591cfb5dde520f9a25f4d
MIPSFPGA User <mipsfpga@mipsfpga-novosibirsk.attlocal.net>
2017-06-11 03:07:56 +0700

    Started adding examples for Terasic DE0-CV board

---
+++ terasic_de0_cv/make_project.bat
@@ -0,0 +1,6 @@
+rd /s /q project
+mkdir project
+
+copy de*.qpf project
+copy de*.qsf project
+copy de*.sdc project
---
+++ terasic_de0_cv/make_project.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+rm -rf project
+mkdir project
+
+cp de*.qpf project
+cp de*.qsf project
+cp de*.sdc project
