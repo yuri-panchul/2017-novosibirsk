7147a5761130bf73a42424c1dede39776917a6cc
yuri.panchul <yuri@panchul.com>
2017-06-21 19:24:48 -0700

    Finalized the example

--- terasic_de10_lite/01_gates/top.v
+++ terasic_de10_lite/01_gates/top.v
@@ -39,14 +39,17 @@ module top
 
     assign led [3] = a ^ b;
 
-    // NAND and NOR gates (useful to explain how a gate is built from
-    // transistors)
- 
-    // Логические элементы И-НЕ и ИЛИ-НЕ (полезны для объяснения,
-    // как логический элемент строится из транзисторов
+    // Building XOR only using AND, OR and NOT
+    
+    // Строим XOR используя только И, ИЛИ, НЕ
 
-    assign led [4] = ~& { a, b };
-    assign led [5] = ~| { a, b };
+    assign led [4] = (a | b) & ~ (a & b);
+
+    // Building NOT by XORing with 1
+    
+    // Строим НЕ, делая ИСКЛЮЧАЮЩЕЕ ИЛИ с единицей
+
+    assign led [5] = a ^ 1'b1;
 
     // De Morgan law illustration
     
@@ -85,8 +88,8 @@ module top_2
     
         led [3] = a ^ b;
 
-        led [4] = ~& { a, b };
-        led [5] = ~| { a, b };
+        led [4] = (a | b) & ~ (a & b);
+        led [5] = a ^ 1'b1;
 
         led [6] = ~ ( a &   b ) ;
         led [7] = ~   a | ~ b   ;
@@ -118,37 +121,48 @@ module top_3
     or   or_1   (led [1], a, b);
     not  not_3  (led [2], a);
     xor  xor_1  (led [3], a, b);
-    nand nand_1 (led [4], a, b);
-    nor  nor_1  (led [5], a, b);
+
+    // led [4] = (a | b) & ~ (a & b)
+
+    wire w1, w2, w3;
+    
+    or   or_2   (w1, a, b);
+    and  and_2  (w2, a, b);
+    not  not_4  (w3, w2);
+    and  and_3  (led [4], w1, w3);
+
+    // led [5] = a ^ 1'b1
+
+    xor  xor_2  (led [5], a, 1'b1);
 
     // led [6] = ~ (a & b)
 
-    wire w1;
+    wire w4;
 
-    and and_2 (w1, a, b);
-    not not_4 (led [6], w1);
+    and  and_4  (w4, a, b);
+    not  not_5  (led [6], w4);
     
     // led [7] = ~ a | ~ b  
 
-    wire w2, w3;
+    wire w5, w6;
 
-    not not_5 (w2, a);
-    not not_6 (w3, b);
-    or  or_2  (led [7], w2, w3);
+    not  not_6  (w5, a);
+    not  not_7  (w6, b);
+    or   or_3   (led [7], w5, w6);
 
     // led [8] = ~ (a | b)
 
-    wire w4;
+    wire w7;
 
-    or  or_3 (w4, a, b);
-    not not_7 (led [8], w4);
+    or   or_4   (w7, a, b);
+    not  not_8  (led [8], w7);
 
     // led [9] = ~ a & ~ b
 
-    wire w5, w6;
+    wire w8, w9;
 
-    not not_8 (w5, a);
-    not not_9 (w6, b);
-    and and_3 (led [9], w5, w6);
+    not  not_9  (w8, a);
+    not  not_10 (w9, b);
+    and  and_5  (led [9], w8, w9);
 
 endmodule
